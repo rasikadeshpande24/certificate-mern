f5dd55464dc502c1bd7db777102c499ba2a970fe
Rasika Deshpande <71489609+rasikadeshpande24@users.noreply.github.com>
2021-07-20 20:27:14 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import certificates from "./images/certificates.png";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+
+import { getPosts } from "./actions/posts";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from "./styles";
+
+const App = () => {
+  const [currentId, setCurrentId] = useState(null);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Certificates
+        </Typography>
+        <img
+          src={certificates}
+          className={classes.image}
+          height="60"
+          alt="certificates"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            className={classes.mainContainer}
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts setCurrentId={setCurrentId} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
